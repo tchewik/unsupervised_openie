d1a3e9d8f423abaa2d9887123dffc231d878e9a0
tchewik <elenachistov@gmail.com>
2019-11-10 19:44:51 +0000

    marked clusters examples

---
+++ clusters_examples.py
@@ -0,0 +1,53 @@
+SAME_CLUSTERS = [[
+        ("victor skumin", "was born on", "30 august 1948"),
+        ("lillia antoinette", "born in", "1998"),
+        ("marionnet", "was born in", "april 2005"),
+        ("chen chunxian", "was born in", "1934"),
+        ("schopenhauer", "was born on", "22 february 1788"),
+    ], [
+        ("justin petersen", "born", "washington d.c."),
+        ("adrian lamo", "was born in", "malden"),
+        ("gower", "was born in", "nottingham"),
+        ("pierre baldi", "born in", "rome"),
+        ('michael levitt', 'was born in', 'south africa'),
+    ], [
+        ("carol bartz", "chairman of", "autodesk"),  # 8
+        ("reddit", "'s ceo'", "steve huffman"),  # 13
+        ("brian farrell", "ceo of", "thq"),  # 13
+        ("guo ping", "is chairman of", "huawei device"),  # 49
+        ("hiroaki nakanishi", "chairman", "hitachi"),  # 49
+    ], [
+        ('mitchell kapor', 'founder of', 'lotus software corporation'),  # 8
+        ('ren zhengfei', 'founded', 'huawei'),  # 8
+        ("parallax", "'s cofounder", "chip gracey"),  # 13
+        ('richard stallman', 'founder of', 'free software foundation'),  # 49
+        ('chad hurley', 'founded', 'youtube'),  # 49
+        ('vidyo', 'was founded by', 'ofer shapiro'),  # 49
+    ], [
+        ('racter', 'was written by', 'william chamberlain'),  # 49
+        ('cyphernomicon', 'written by', 'timothy c. may'),  # 49
+        ('weizenbaum', 'wrote', 'eliza'),  # 9
+        ('weizenbaum', 'first implemented', 'eliza'),  # 11
+        ('weizenbaum', 'originally wrote', 'eliza'),  # 19
+        ('dirhash', 'was implemented by', 'ian dowse'),  # 49
+        ('perl', 'was developed by', 'larry wall'),
+    ], [
+        ('microtox', 'was developed', '1979'),  # 45
+        ('perl', 'was developed', '1987'),  # 26
+        ('four-corner method', 'was developed in', '1920s'),  # 45
+        ('natural color system', 'developed in', '1979'),  # 45
+    ], [
+        ('joseph f. ossanna', 'worked', 'bell telephone laboratories'),  # 6
+        ('seymour cray', 'worked at', 'cdc'),  # 49
+        ('david mcfarland', 'has worked at', 'digitek'),  # 13
+        ('david blythe', 'worked at', 'microsoft'),  # 13
+        ('doug ahl', 'had worked at', 'digitek'),  # 13
+    ], [
+        ('msn messenger service', 'was launched in', '1999'),  # 4
+        ('tumblr', 'launched', '2007'),  # 8
+        ('windows live spaces', 'was launched in', 'december 2004'),  # 45
+        ('youtube', 'was launched in', '2005'),  # 45
+        ('wikipedia', 'was launched in', 'january 2001'),  # 45
+        ('disinfopedia', 'was launched', 'march 2003'),  # 22
+    ]
+]
